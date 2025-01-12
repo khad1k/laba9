aa4dfee3cdd47bd70253a221bd721326a82e498c
khad1k <andreytv@icloud.com>
2025-01-12 19:45:39 +0300

    Add files via upload

---
+++ laba1.py
@@ -0,0 +1,11 @@
+# Задача №1
+number = int(input('Введите число: '))
+for i in range(1, number + 1):
+    print(i)
+
+
+# Задача №2
+num1 = int(input('Введите первое число: '))
+num2 = int(input('Введите второе число: '))
+print('Большее число:', max(num1, num2))
+
